c321fc0f4a6d65e6dea9ab77b641c7c941123dc0
crocka <chino123abc@hotmail.com>
2022-03-07 22:50:06 +0000

    Implement exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+sum = Store.sum(:annual_revenue)
+puts sum
+
+numOfStores = Store.count(:id)
+
+puts sum / numOfStores
+
+puts Store.where("annual_revenue > 1000000").count(:id)
