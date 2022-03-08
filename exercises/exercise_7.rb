7b7b0a41fa2b7ebc315e6b0aafbb0f9a0cbbee31
crocka <chino123abc@hotmail.com>
2022-03-08 00:03:41 +0000

    Implement exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,5 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
-store = Store.new(name: "Hong Kong")
-store.save!
+store = Store.new(name: "Hong", annual_revenue: -1231)
+em = Employee.new(first_name: "Fhur", last_name: "Firan", hourly_rate: 10)
+store.save;
+em.save;
+
+puts store.errors.full_messages
+puts em.errors.full_messages
